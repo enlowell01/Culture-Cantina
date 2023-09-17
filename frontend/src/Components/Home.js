3679a8fe502952f93f5877ce02dbc05bde1c794a
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-09-17 13:29:14 -0400

    Completed fetching data from api. Getting title and image and both are being displayed. Need styling and added layers to page.

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,51 @@
+import { useState, useEffect} from "react"
+
+
+function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDBiMGNjMTExZDQzNWIyMzM3ZGE2MDc0NzIxZTBkMyIsInN1YiI6IjY1MDcwYzhhMTA5ZGVjMDEwY2MyMDk0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X8PHXGzgvcepy3Uc808zPOYuKIPVNQGTGkwHntgUh78'
+        }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result)
+      setMovies(result.results)
+    } catch (error) {
+      
+    }
+      
+  }
+
+
+    fetchData()
+  }, []);
+
+  const imageUrl = 'https://image.tmdb.org/t/p/original'
+  const display = movies.map(movie => {
+    return (
+      <div key={movie.id}>
+        {[movie.title]}
+        <img src={`${imageUrl}${movie.backdrop_path}`} />
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Movies?</h1>
+      {display}
+    </div>
+  )
+}
+
+export default Home;
