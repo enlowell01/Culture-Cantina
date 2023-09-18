9fbd66c090f51bf2916f57957ed555a1b25922f0
L-delara <ligia.delara15@gmail.com>
2023-09-18 18:21:12 -0400

    Cleaned up some files. Made images smaller
    Images were taking up whole page. Hard to view. Will try to replicate for a tv page later

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,51 +1,48 @@
-import { useState, useEffect} from "react"
-
+import { useState, useEffect } from "react";
 
 function Home() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'
+      const url =
+        "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
       const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDBiMGNjMTExZDQzNWIyMzM3ZGE2MDc0NzIxZTBkMyIsInN1YiI6IjY1MDcwYzhhMTA5ZGVjMDEwY2MyMDk0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X8PHXGzgvcepy3Uc808zPOYuKIPVNQGTGkwHntgUh78'
-        }
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDBiMGNjMTExZDQzNWIyMzM3ZGE2MDc0NzIxZTBkMyIsInN1YiI6IjY1MDcwYzhhMTA5ZGVjMDEwY2MyMDk0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X8PHXGzgvcepy3Uc808zPOYuKIPVNQGTGkwHntgUh78",
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        setMovies(result.results);
+      } catch (error) {}
     };
-    
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result)
-      setMovies(result.results)
-    } catch (error) {
-      
-    }
-      
-  }
 
-
-    fetchData()
+    fetchData();
   }, []);
 
-  const imageUrl = 'https://image.tmdb.org/t/p/original'
-  const display = movies.map(movie => {
+  const imageUrl = "https://image.tmdb.org/t/p/w500";
+  const display = movies.map((movie) => {
     return (
       <div key={movie.id}>
         {[movie.title]}
-        <img src={`${imageUrl}${movie.backdrop_path}`} />
+        <img src={`${imageUrl}${movie.backdrop_path}`} alt={movie.title} />
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div>
       <h1>Movies?</h1>
       {display}
     </div>
-  )
+  );
 }
 
 export default Home;
