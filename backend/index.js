217b824d9573cb7d0eeb1d44dd2f88b87caf2849
Evan Lowell <99279024+enlowell01@users.noreply.github.com>
2023-09-26 21:43:54 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'https://culture-cantina-5yju.vercel.app/' }));
+app.use(cors({ credentials: true, origin: 'https://culture-cantina-5yju.vercel.app' }));
 app.use(cookieParser());
 
 // Routes
