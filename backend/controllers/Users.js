ac56de5a4ab9ec4f018bc62ac374af41f4d39249
Evan <evanlowell01@gmail.com>
2023-09-27 14:57:20 -0400

    more

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -85,7 +85,7 @@ async function userLogin(req, res) {
   
         // Set the token as a cookie in the response
         res.cookie('token', token, {
-          httpOnly: false, // Helps protect against XSS attacks
+          httpOnly: true, // Helps protect against XSS attacks
           secure: process.env.NODE_ENV === 'production', // Requires HTTPS in production
           sameSite: 'none', // Helps prevent CSRF attacks
         });
