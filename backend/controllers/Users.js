0701016d8fdedbb26cfd797442cc03ada3f2a894
Evan Lowell <99279024+enlowell01@users.noreply.github.com>
2023-09-27 19:13:36 -0400

    Update Users.js

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -85,9 +85,9 @@ async function userLogin(req, res) {
   
         // Set the token as a cookie in the response
         res.cookie('token', token, {
-          maxAge: 2 * 60 * 60 * 1000,  
           httpOnly: true, // Helps protect against XSS attacks
           secure: true, //process.env.NODE_ENV === 'production', // Requires HTTPS in production
+          maxAge: 2 * 60 * 60 * 1000,  
         });
         
         res.json({ id: userDoc._id, username });
