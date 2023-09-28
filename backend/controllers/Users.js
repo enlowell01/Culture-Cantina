68f9692befc0dd1ba7c52a645dd349be8c0a9150
Evan Lowell <99279024+enlowell01@users.noreply.github.com>
2023-09-27 20:28:27 -0400

    Update Users.js

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -102,7 +102,7 @@ async function userLogin(req, res) {
   }
 async function userLogout(req, res) {
   res.cookie('token', '', {
-          maxAge: 0 // clears cookie
+          maxAge: 0, // clears cookie
           sameSite: 'none',
           httpOnly: true, // Helps protect against XSS attacks
           secure: true, //process.env.NODE_ENV === 'production', // Requires HTTPS in production
